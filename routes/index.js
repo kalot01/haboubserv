f14c7ccb0d9155888ed036a8e5bd9f1879a49960
Melkart  JuniorEnterprise <projets.melkart.je@gmail.com>
2021-04-03 15:36:20 +0100

    finish

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,35 @@ var router = express.Router();
 var con = require("../config/dbConfig");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.post("/", function (req, res, next) {
+  const sql = `SELECT * FROM krahebautorise WHERE numero = "${req.body.num}" AND serie = "${req.body.serie}" `;
+  con.query(sql, function (err, results) {
+    if (err) {
+      console.log("err");
+      res.json({ err: err });
+    } else {
+      if (results.length > 0) {
+        const sql = `INSERT INTO autorisesdakhlou(idcar) VALUES (${results[0].id})`;
+        con.query(sql, function (err, results) {
+          if (err) {
+            console.log("err");
+            res.json({ err: err });
+          } else {
+            // code ba3then signal besh beb yethal
+          }
+        });
+      } else {
+        const sql = `INSERT INTO krahebnonautorises(serie, num) VALUES ("${req.body.num}" , "${req.body.serie}")`;
+        con.query(sql, function (err, results) {
+          if (err) {
+            console.log("err");
+            res.json({ err: err });
+          }
+        });
+      }
+    }
+  });
+  res.send(true);
 });
 
 module.exports = router;
